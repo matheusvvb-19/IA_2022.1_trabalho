88b9a34148fccd5d3cbc20b0c14e8218c7ae65bc
Pedrohme <pedrohmen45@gmail.com>
2022-08-17 16:57:39 -0300

    estrutura inicial do projeto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IA_2022.1_trabalho
 Trabalho em grupo realizado durante a disciplina de Inteligência Artificial (IA) no período letivo 2022.1 da UFSCar Sorocaba.
+
+[Especificação](https://docs.google.com/document/d/1L5gU47DqkQrit6gDEnjsdW7Njj6dpSWuWfL082h5RHI/edit)
---
+++ src/algoritmos.py
@@ -0,0 +1,34 @@
+import typing
+
+
+def k_means(dataset: typing.TextIO, k: int, it: int):
+    """ Algoritmo K-Médias
+        args:
+        dataset -- arquivo contento o conjunto de dados
+        k -- número de clusters desejado
+        it -- número de iterações desejado
+    """
+
+    pass
+
+
+def single_link(dataset: typing.TextIO, k_min: int, k_max: int):
+    """ Algoritmo Single-Link
+        args:
+        dataset -- arquivo contento o conjunto de dados
+        k_min -- número inicial do intervalo do número de clusters
+        k_max -- número final do intervalo do número de clusters
+    """
+
+    pass
+
+
+def complete_link(dataset: typing.TextIO, k_min: int, k_max: int):
+    """ Algoritmo Complete-Link
+        args:
+        dataset -- arquivo contento o conjunto de dados
+        k_min -- número inicial do intervalo do número de clusters
+        k_max -- número final do intervalo do número de clusters
+    """
+
+    pass
