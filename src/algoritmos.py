15e79c8fed8e4cd6ff307930bcf24267963c49fe
“matheusvvb-19” <matheus.berto1@hotmail.com>
2022-08-19 00:33:06 -0300

    kmeans
    Co-authored-by: Pedro Henrique Mendes <Pedrohme@users.noreply.github.com>

--- src/algoritmos.py
+++ src/algoritmos.py
@@ -1,4 +1,6 @@
-import typing
+import typing, random
+import pandas as pd
+import numpy as np
 
 
 def k_means(dataset: typing.TextIO, k: int, it: int):
@@ -9,7 +11,25 @@ def k_means(dataset: typing.TextIO, k: int, it: int):
         it -- número de iterações desejado
     """
 
-    pass
+    # leitura dos dados:
+    df = pd.read_csv(dataset, sep="\t")
+
+    # captura do intervalo de valores de cada atributo (coluna de dados) do dataset:
+    intervalosPorAtributo = []
+    for col in df.columns[1:]:
+        intervalo = df[col].agg(['min', 'max'])
+        intervalosPorAtributo.append(intervalo)
+
+    # escolha aleatória dos centróides iniciais:
+    centroides = []
+    for i in range(k):
+        ponto = ()
+        for intervalo in intervalosPorAtributo:
+            ponto += (random.randint(intervalo[0], intervalo[1]), )
+        
+        centroides.append(ponto)
+
+    
 
 
 def single_link(dataset: typing.TextIO, k_min: int, k_max: int):
@@ -32,3 +52,9 @@ def complete_link(dataset: typing.TextIO, k_min: int, k_max: int):
     """
 
     pass
+
+def euclidean_dist(point, data):
+    """ Euclidean distance between point & data.
+        Point has dimensions (m,), data has dimensions (n,m), and output will be of size (n,).
+    """
+    return np.sqrt(np.sum((point - data)**2, axis=1))
